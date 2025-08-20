7bae23c37eaf33cfe3bdc87dabb848263d402ce5
VladyslavKuzenko <dalbayb281@gmail.com>
2025-08-20 14:50:56 +0200

    Section 4 and Edited Section 3

---
+++ Section 4/07-tic-tac-toe-starting-project/src/components/GameOver.jsx
@@ -0,0 +1,10 @@
+export default function GameOver({ winner,onResetGame }) {
+  return (
+    <div id="game-over">
+      <h2>Game Over!</h2>
+      {winner &&<p>{winner} win!</p>}
+      {!winner &&<p>It`s a draw!</p>}
+      <p><button onClick={onResetGame}>Rematch!</button></p>
+    </div>
+  );
+}
